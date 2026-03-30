42bec694ff722432c7dab4f632084097df341471
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-03-30 16:33:56 +0530

    Add UserRequestDTO with validation annotations

---
+++ backend/src/main/java/com/mocktestpro/module/user/dto/UserRequestDTO.java
@@ -0,0 +1,26 @@
+package com.mocktestpro.module.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRequestDTO {
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 100, message = "First name must be 2-100 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 100, message = "Last name must be 2-100 characters")
+    private String lastName;
+
+    @Size(max = 500, message = "URL cannot exceed 500 characters")
+    private String profilePictureUrl;
+
+    private String phoneNumber;
+}
